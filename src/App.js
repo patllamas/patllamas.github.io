4b973e41040298f202b2a2b445075559ffa211c7
Patrick <74840936+patllamas@users.noreply.github.com>
2021-09-16 02:20:18 -0400

    Polished up most of the page and added links to github when clicking on portfolio items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Projects from "./Projects";
 
+Projects.defaultProps = {
+  icon2: '',
+}
+
 function App() {
   return (
     <div className="App">
-      <div id='wallpaper' className="wallpaper" data-image='.\images\hitoshi-namura-MyiYvI-EKis-unsplash.jpg'></div>
+      <div id='wallpaper' className="wallpaper" data-image='.images/vlad-tchompalov-dHjV9czPNHs-unsplash.jpg'></div>
       <div className="content">
         <aside className="side">
           <figure id='picture' className="picture">
@@ -19,21 +23,26 @@ function App() {
           </figure>
         </aside>
         <main className="about">
-          <h1 className="name">Hey, I'm Patrick Llamas</h1>
+          <h1 className="name">Hi, I'm Patrick</h1>
           <p className="job">Web Developer</p>
           <hr className="hr" />
+          <div className="languages"></div>
           <div className="description">
-            <p>I build websites and possess a passion for music and games</p>
+            <p>
+              Aspiring web developer with a background in information systems. 
+              Familiar with React JS, Django, Python, JavaScript, HTML, and CSS.
+              Enjoys self-improvement in all shapes and forms.
+            </p>
           </div>
           <div className="contact">
-            <a className="button" href="mailto:pat.llamas@outlook.com">
-              Let's talk
+            <a className="button" href="https://www.linkedin.com/in/patllamas/">
+              Let's Connect
             </a>
           </div>
           <ul className="social">
-            <li>
-              <a href='https://github.com/patllamas/'>
-                <i className="fab fa-github"></i>
+          <li>
+              <a href='mailto:pat.llamas@outlook.com'>
+                <i className="fas fa-at"></i>
               </a>
             </li>
             <li>
@@ -41,26 +50,40 @@ function App() {
                 <i className="fab fa-linkedin"></i>
               </a>
             </li>
+            <li>
+              <a href='https://github.com/patllamas/'>
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+
+
           </ul>
         </main>
       </div>
       <div className='CardGroup'>
           <Projects 
-            title="Zerazal Photography" 
-            jobtitle="Front End Developer" 
+            title="Zerazal Photography"
+            link="https://github.com/patllamas/Marium-Photography#readme" 
+            jobtitle="Front End Developer"
+            languages={[<i style={{fontSize: 1.2 + "rem"}} class="fab fa-react"></i>, " ",<i style={{fontSize: 1.2 + "rem"}} class="fab fa-html5"></i>," ",<i style={{fontSize: 1.2 + "rem"}} class="fab fa-css3-alt"></i>," ", <i style={{fontSize: 1.2 + "rem"}} class="fab fa-js-square"></i>]}
             image={require(`../src/images/zerazal.png`).default} 
           />
           <Projects
             title="Lord Fairfax Pet Motel"
+            link="https://github.com/patllamas/lordfairfax#lord-fairfax-pet-motel"
             jobtitle="Full Stack Developer"
+            languages={[<i style={{fontSize: 1.2 + "rem"}} class="fab fa-python"></i>, " ",<i style={{fontSize: 1.2 + "rem"}} class="fab fa-html5"></i>," ",<i style={{fontSize: 1.2 + "rem"}} class="fab fa-css3-alt"></i>," ", <i style={{fontSize: 1.2 + "rem"}} class="fab fa-js-square"></i>]}
             image={require(`../src/images/lord_fairfax.png`).default}
           />
           <Projects
-            title="Lord Fairfax Pet Motel"
-            jobtitle="Full Stack Developer"
+            title="coolbot"
+            link='https://github.com/patllamas/coolbot/tree/main#coolbot'
+            jobtitle="Software Developer"
+            languages={[" ", <i style={{fontSize: 1.2 + "rem"}} class="fab fa-js-square"></i>," ", <i style={{fontSize: 1.2 + "rem"}} class="fab fa-discord"></i>]}
             image={require(`../src/images/coolbot.png`).default}
           />
         </div>
+        <p style={{textAlign: 'center'}}>Page by Pat 2021 <i class="fab fa-connectdevelop fa-spin" /> </p>
     </div>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./Projects.css";
 
+
+
 class Projects extends React.Component {
   render(props) {
+
     return (
       <div className='column' className="Projects">
-        <h3>{this.props.title}</h3>
-        <p>{this.props.jobtitle}</p>
-        <img
-          // class="image is-128x128"
-          src={this.props.image}
-        />
+        <a href={this.props.link}>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.jobtitle} | {this.props.languages}
+          </p>
+          <img 
+            // class="image is-128x128"
+            src={this.props.image}
+          />
+        </a>
       </div>
     );
   }
 }
+
+
 
 export default Projects;
