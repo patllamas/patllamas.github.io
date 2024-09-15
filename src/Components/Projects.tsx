3209e657f8a7409f0fea7437d5946284ce86d655
patllamas <74840936+patllamas@users.noreply.github.com>
2024-09-14 22:23:00 -0400

    implemented react carousel

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
-import { Box, Button, Dialog, IconButton, Typography } from "@mui/material";
-import Project from "./Project";
+import {
+  Box,
+  Button,
+  Dialog,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Project from "./Project";
 
 const Projects = (props: any) => {
   const [currentProject, setCurrentProject] = useState<string>("");
@@ -13,67 +21,59 @@ const Projects = (props: any) => {
   };
   const handleClose = () => setOpenModal(false);
 
+  var projects = [
+    {
+      image: "barhaus",
+      name: "Barhaus Bastion",
+      year: "2024",
+      description:
+        "Led a team of 4 to create a hand drawn, 2D tower defense game using the Godot Engine within 1 week for the Migjam 22. Designed and programmed enemies, towers, construction, player unit, and win conditions.",
+      technology: "Godot, GDScript, Git",
+    },
+    {
+      image: "amara",
+      name: "Amara",
+      year: "2023",
+      description:
+        "Led a team of 8 to create a hand drawn, 2D adventure game using the Godot Engine within 1 month.",
+      technology: "Godot, GDScript, Git",
+    },
+    {
+      image: "dowehaveliftoff",
+      name: "Do we Have Liftoff?",
+      year: "2023",
+      description:
+        "Solo project for a 3 day game jam. A space horror FPS game based on Dead Space and Resident Evil with a retro aesthetic.",
+      technology: "Godot, GDScript, Git",
+    },
+    {
+      image: "babyrun",
+      name: "Baby Run",
+      year: "2024",
+      description: "Created an infinite stairs type ",
+      technology: "Godot, GDScript, Git",
+    },
+  ];
+
   return (
     <div style={{ textAlign: "center", userSelect: "none" }}>
-      <Typography sx={{ typography: { xs: "h2", md: "h1" } }}>
-        Things I've done.
+      <Typography sx={{ typography: { xs: "h3", md: "h2" } }}>
+        Games and projects I've created.
       </Typography>
-      <div style={{ fontSize: "35px" }}>
-        <Button
-          onClick={() => handleOpen("amara")}
-          variant="text"
-          sx={{
-            fontSize: "35px",
-            margin: "5px",
-            fontWeight: "bold",
-            color: "#592F52",
-          }}
-        >
-          Amara
-        </Button>
-        |
-        <Button
-          onClick={() => handleOpen("dowehaveliftoff")}
-          variant="text"
-          sx={{
-            fontSize: "35px",
-            margin: "5px",
-            fontWeight: "bold",
-            color: "#592F52",
-          }}
-        >
-          Do We Have Liftoff?
-        </Button>
-        |
-        <Button
-          onClick={() => handleOpen("lordfairfax")}
-          variant="text"
-          sx={{
-            fontSize: "35px",
-            margin: "5px",
-            fontWeight: "bold",
-            color: "#592F52",
-          }}
-        >
-          Lord Fairfax
-        </Button>
-        |
-        <Button
-          onClick={() => handleOpen("coolbot")}
-          variant="text"
-          sx={{
-            fontSize: "35px",
-            margin: "5px",
-            fontWeight: "bold",
-            color: "#592F52",
-          }}
-        >
-          coolbot
-        </Button>
+      <div
+        style={{
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width:"40%"
+        }}
+      >
+        <Carousel sx={{ maxWidth: "600px" }}>
+          {projects.map((project, i) => (
+            <Project projectInfo={project} />
+          ))}
+        </Carousel>
       </div>
-      <Dialog open={openModal} onClose={handleClose}>
-        <Project currentProject={currentProject} />
-      </Dialog>
       <IconButton
         sx={{
           left: "50%",
